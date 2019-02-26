0cca2b3ceba5ea04ea169a3018570965a9bd0003
meowcodes <meowcodes@gmail.com>
2019-02-26 11:55:42 -0800

    add User model

--- models.py
+++ models.py
@@ -6,3 +6,18 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """ User Model """
+
+    username = db.Column(db.String(20),
+                         primary_key=True)
+    password = db.Column(db.Text, 
+                         nullable=False)
+    email = db,Column(db.String(50),
+                      nullable=False,
+                      unique=True)
+    first_name = db.Column(db.String(30),
+                           nullable=False)
+    last_name = db.Column(db.String(30),
+                          nullable=False)
